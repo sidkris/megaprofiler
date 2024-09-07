7d79196d10ee3036e0231ae37ae32ebf6852bdb7
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-09-07 19:10:57 +0530

    Update megaprofiler.py

--- megaprofiler/megaprofiler.py
+++ megaprofiler/megaprofiler.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 from data_validator import DataValidator as dv 
 from report_generator import ReportGenerator as rg
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 class MegaProfiler:
 
@@ -21,11 +25,52 @@ class MegaProfiler:
         return profile
 
 
+    # @classmethod
+    # def summarize(cls, data, rules):
+
+    #     profile_ = cls.profile(data)
+    #     profile_report = rg.generate_report(profile_)
+    #     rule_violations = dv.validate(data, rules)
+
+    #     return profile_report, rule_violations
+    
+
     @classmethod
-    def summarize(cls, data, rules):
+    def pearson_correlation_analysis(cls, data):
+        """Returns the correlation matrix for numerical columns."""
+        return data.corr(method = "pearson")
+    
 
-        profile_ = cls.profile(data)
-        profile_report = rg.generate_report(profile_)
-        rule_violations = dv.validate(data, rules)
+    @classmethod
+    def covariance_analysis(cls, data):
+        """Returns the covariance matrix for numerical columns."""
+        return data.cov()
+    
 
-        return profile_report, rule_violations
+    @classmethod
+    def missing_data_heatmap(cls, data):
+        """Generate a heatmap of missing values in the dataset."""
+        plt.figure(figsize = (10, 7))
+        sns.heatmap(data.isnull(), cba = False, cmap = "viridis")
+        plt.title("-- MISSING DATA HEATMAP --")
+        plt.show()
+
+
+    @classmethod
+    def zscore_outlier_analysis(data, threshold = 3):
+        """Detect outliers using z-score."""
+        z_scores = np.abs(stats.zscore(data.select_dtypes(include = [np.number])))
+        outliers = (z_scores > threshold).any(axis = 1)
+        
+        return data[outliers]
+    
+
+    @classmethod
+    def iqr_outlier_analysis(data):
+        """Detect outliers using IQR (Inter-Quartile Range)."""
+        q1 = data.quantile(0.25)
+        q3 = data.qunatile(0.75)
+        iqr = q3 - q1
+
+        return data[((data < (q1 - 1.5 * iqr)) | (data > (q3 + 1.5 * iqr))).any(axis = 1)]
+
